75d94ec93df5c833a07a5bd3a94be1d73b426574
Yan Gustavo Pegyn Silva <46832714+yanpegyn@users.noreply.github.com>
2021-02-05 10:19:18 -0300

    Models

---
+++ database/migrations/2021_02_05_131824_create_produtos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('categoria_id')->constrained();
+            $table->string('nome');
+            $table->decimal('preco',9,2); //Precisão de 9 e 2 casas decimais
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
